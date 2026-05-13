a60f02ec555c7381ec26c9c77244c8942353db53
Abakarix4 <abakarix4@gmail.com>
2026-05-13 16:12:44 +0100

    SEO Friendly

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,23 +1,28 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Article
-from django.contrib.sitemaps import Sitemap
+from django.utils.translation import activate
 from django.urls import reverse
 
 class ArticleSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.9
+    i18n = True
+    languages = ['fr', 'en', 'ar']
 
     def items(self):
         return Article.objects.all()
 
     def lastmod(self, obj):
         return obj.date_modification
-    
+
 
 
 class StaticSitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.5
+    i18n = True  # génère une entrée par langue automatiquement
+    languages = ['fr', 'en', 'ar']
+    alternates = True
 
     def items(self):
         return [
